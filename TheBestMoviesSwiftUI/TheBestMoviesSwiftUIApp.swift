ff5661773cb4fe6f2c97926f0dfb652137be343c
dahdouhMouad <mouaddhd10@gmail.com>
2023-09-28 14:32:54 +0100

    Initial Commit

---
+++ TheBestMoviesSwiftUI/TheBestMoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBestMoviesSwiftUIApp.swift
+//  TheBestMoviesSwiftUI
+//
+//  Created by Macbook Pro on 28/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheBestMoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
